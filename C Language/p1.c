6ec0406b07c10d9a3aad8007533610297d9846e1
Yogeshwar Chaudhari <108257281+YogeshwarChaudhariii@users.noreply.github.com>
2025-07-21 04:01:04 +0530

    Update p1.c

--- C Language/p1.c
+++ C Language/p1.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<string.h>
+#include<stdlib.h>
 
 int main()
 {
-    FILE *fp;           //File pointer
+    FILE *fp;                               //File pointer
+    FILE *SourceFile;                       //Source File
+    FILE *DestinationFile;                  //Desination file
+
     int choice = 0;
+
     char FileName[20] = {'\0'};
+    char SourFileName[100] = {'\0'};            //Source File
+    char DesFileName[100] = {'\0'};             //Desination file
+
+    char ch;
 
     printf("1. Create a new file...\n");
     printf("2. Delete the file...\n");
     printf("3. Write a content in file\n");
     printf("4. Check file or Directory exist or not\n");
+    printf("5. Copy data from Source file to other file\n");
     printf("Enter your choice: \n");
     scanf("%d",&choice);
 
@@ -76,12 +86,38 @@ int main()
         }
         break;
 
+    case 5:
+        printf("Enter Source file name: \n");
+        scanf("%s",&SourFileName);
+        
+        printf("Enter Destination file name: \n");
+        scanf("%s",&DesFileName);
+        
+        SourceFile = fopen(SourFileName, "r");
+        DestinationFile = fopen(DesFileName, "w");
+        
+        if ((SourceFile == NULL) || (DestinationFile == NULL))
+        {
+            printf("File not exist");
+            exit(EXIT_FAILURE);                 //if file not exit then exit 
+        }
+        else
+        {
+            while ((ch = fgetc(SourceFile)) != EOF)         //EOF = End of file
+            {
+                fputc(ch, DestinationFile);
+            }
+            printf("Data copy sucessfully\n");
+        }
+        break;
+
             default:
             break;
         }
 
 
     fclose(fp);
+    
     
     return 0;
 }
